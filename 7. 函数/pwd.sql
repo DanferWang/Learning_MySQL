038138726d271dbff1e7617b99759efd5087a3c6
DanferWang <42688937+DanferWang@users.noreply.github.com>
2021-09-27 19:26:04 +0200

    shade

--- 7. 函数/pwd.sql
+++ 7. 函数/pwd.sql
@@ -3,7 +3,7 @@ SELECT USER() FROM DUAL
 
 SELECT DATABASE()
 
-SELECT MD5('`123456danfer') FROM DUAL
+SELECT MD5('`123456***danfer') FROM DUAL
 
 -- 常用于保护用户密码的应用中
 CREATE TABLE register_user(id INT,
@@ -11,13 +11,13 @@ CREATE TABLE register_user(id INT,
 														pwd CHAR(32) NOT NULL DEFAULT '')
 
 INSERT INTO register_user
-				VALUES(1, '王浩辰', MD5('123456danfer'))
+				VALUES(1, '王一二', MD5('123456***danfer'))
 
 SELECT * FROM register_user
 
 SELECT * FROM register_user
-					WHERE `name`='王浩辰' AND pwd=MD5('123456danfer')
+					WHERE `name`='王一二' AND pwd=MD5('123456***danfer')
 
 -- MySQL数据库中的用户密码是用PASSWORD函数加密
-SELECT PASSWORD('083432danfer') FROM DUAL -- *914654D500504734C4417E9ADA832616E0590316
+SELECT PASSWORD('123456***danfer') FROM DUAL -- *914654D500504734C4417E9ADA832616E0590316
 SELECT * FROM mysql.user
